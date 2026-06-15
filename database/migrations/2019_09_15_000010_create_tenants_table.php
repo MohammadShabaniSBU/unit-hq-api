5f91bfa7699aa0bc2d22ec0cf99812d04bf2060b
Mohammad <maintenance@ffrw.es>
2026-06-15 16:47:28 +0330

    feat: add new packages

--- database/migrations/2019_09_15_000010_create_tenants_table.php
+++ database/migrations/2019_09_15_000010_create_tenants_table.php
@@ -1,26 +1,37 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateTenantsTable extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up(): void
     {
         Schema::create('tenants', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('slug')->unique();
-            $table->string('db_connection');
-            $table->string('stripe_connect_account_id')->nullable()->unique();
-            $table->string('stripe_onboarding_status')->default('pending');
+            $table->string('id')->primary();
+
+            // your custom columns may go here
+
             $table->timestamps();
+            $table->json('data')->nullable();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down(): void
     {
         Schema::dropIfExists('tenants');
     }
-};
+}
